e108dc3e5fa0c9c4af1c2046bcc987fb60915bbc
fauziUI <agen46.web@gmail.com>
2022-09-30 18:58:19 +0700

    apply react-router

---
+++ src/pages/create.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import image_TP from "../assets/Party.png";
+import { InputText } from "../components/inputText";
+import { InputDate } from "../components/inputDate";
+
+export const Create = () => {
+  // const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState();
+  const [state, setState] = useState({
+    eventName: "",
+    email: "",
+    noHP: "",
+    noIdentitas: "",
+    noNPWP: "",
+  });
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    const name = evt.target.name;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  }
+
+  function onClick(event) {}
+
+  const fileSelectedHandler = (event) => {
+    if (event.target.files[0]) {
+      let preview = document.getElementById("imgPreview");
+      let src = URL.createObjectURL(event.target.files[0]);
+      // setLink(src);
+      // setFotoKTP(true);
+    }
+
+    setState({
+      ...state,
+    });
+  };
+
+  return (
+    <div className="bg-white w-full h-full min-h-screen p-8">
+      <div className="md:flex justify-center lg:py-20">
+        <div className="text-center">
+          <img
+            src={image_TP}
+            alt="landing_page_image"
+            className="cursor-pointer m-auto h-96 md:mx-12 "
+          />
+          <input
+            className="my-4"
+            type="file"
+            name="photo-ttd"
+            onChange={fileSelectedHandler}
+            accept="image/png, image/gif, image/jpeg"
+            capture="environment"
+          />
+        </div>
+        <div className="lg:w-1/2 max-w-2xl md:my-0 my-12">
+          <InputText
+            titel="🎉 Event Name"
+            name="event"
+            value={state.namaLengkap}
+            onChange={handleChange}
+          />
+          <InputText
+            titel="🔗 Host Name"
+            name="event"
+            value={state.namaLengkap}
+            onChange={handleChange}
+          />
+          <InputDate
+            titel="✏️ Starts at"
+            selected={startDate}
+            onChange={setStartDate}
+          />
+          <InputDate
+            titel="🏁 Ends at"
+            selected={startDate}
+            onChange={setStartDate}
+          />
+          <InputText
+            titel="📍 Location"
+            name="event"
+            value={state.namaLengkap}
+            onChange={handleChange}
+          />
+          <div className="flex justify-center my-8">
+            <button className=" bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-3 px-10 lg:px-16 md:px-8 rounded-xl text-white text-xl">
+              🎉 Create my event
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* <Home /> */}
+    </div>
+  );
+};
